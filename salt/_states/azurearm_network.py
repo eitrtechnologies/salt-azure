d98520b8e6699feb42d4082fd21ecfaad9887468
nicholasmhughes <nicholasmhughes@gmail.com>
2019-07-02 02:32:48 +0000

    adding vnet peering states

--- salt/_states/azurearm_network.py
+++ salt/_states/azurearm_network.py
@@ -90,6 +90,7 @@ Azure (ARM) Network State Module
 # Python libs
 from __future__ import absolute_import
 import logging
+import re
 
 # Salt libs
 try:
@@ -2511,4 +2512,274 @@ def route_absent(name, route_table, resource_group, connection_auth=None):
         return ret
 
     ret['comment'] = 'Failed to delete route {0}!'.format(name)
+    return ret
+
+
+def virtual_network_peering_present(name, remote_virtual_network, virtual_network, resource_group,
+                                    remote_vnet_group=None, allow_virtual_network_access=True,
+                                    allow_forwarded_traffic=False, allow_gateway_transit=False,
+                                    use_remote_gateways=False, connection_auth=None, **kwargs):
+    '''
+    .. versionadded:: Sodium
+
+    Ensure a virtual network peering object exists.
+
+    :param name:
+        Name of the peering object.
+
+    :param remote_virtual_network:
+        The name of the remote virtual network.
+
+    :param remote_vnet_group:
+        The resource group of the remote virtual network. Defaults to the same resource group
+        as the local virtual network.
+
+    :param virtual_network:
+        Name of the existing virtual network to contain the peering object.
+
+    :param resource_group:
+        The resource group assigned to the local virtual network.
+
+    :param allow_virtual_network_access:
+        Whether the VMs in the local virtual network space would be able to access
+        the VMs in remote virtual network space.
+
+    :param allow_forwarded_traffic:
+        Whether the forwarded traffic from the VMs in the local virtual network will
+        be allowed/disallowed in remote virtual network.
+
+    :param allow_gateway_transit:
+        If gateway links can be used in remote virtual networking to link to this virtual network.
+
+    :param use_remote_gateways:
+        If remote gateways can be used on this virtual network. If the flag is set to true, and allow_gateway_transit
+        on remote peering is also true, virtual network will use gateways of remote virtual network for transit.
+        Only one peering can have this flag set to true. This flag cannot be set if virtual network already has a
+        gateway.
+
+    :param connection_auth:
+        A dict with subscription and authentication parameters to be used in connecting to the
+        Azure Resource Manager API.
+
+    Example usage:
+
+    .. code-block:: yaml
+
+        Ensure virtual network peering exists:
+            azurearm_network.virtual_network_peering_present:
+                - name: vnet1_to_vnet2
+                - virtual_network: vnet1
+                - resource_group: group1
+                - remote_virtual_network: vnet2
+                - remote_vnet_group: group2
+                - allow_virtual_network_access: True
+                - allow_forwarded_traffic: False
+                - allow_gateway_transit: False
+                - use_remote_gateways: False
+                - connection_auth: {{ profile }}
+                - require:
+                  - azurearm_network: Ensure virtual network exists
+                  - azurearm_network: Ensure remote virtual network exists
+
+    '''
+    ret = {
+        'name': name,
+        'result': False,
+        'comment': '',
+        'changes': {}
+    }
+
+    if not isinstance(connection_auth, dict):
+        ret['comment'] = 'Connection information must be specified via connection_auth dictionary!'
+        return ret
+
+    peering = __salt__['azurearm_network.virtual_network_peering_get'](
+        name,
+        virtual_network,
+        resource_group,
+        azurearm_log_level='info',
+        **connection_auth
+    )
+
+    if 'error' not in peering:
+        remote_vnet = None
+        if peering.get('remote_virtual_network', {}).get('id'):
+            remote_vnet = peering['remote_virtual_network']['id'].split('/')[-1]
+
+        if remote_virtual_network != remote_vnet:
+            ret['changes']['remote_virtual_network'] = {
+                'old': remote_vnet,
+                'new': remote_virtual_network
+            }
+
+        for bool_opt in ['use_remote_gateways', 'allow_forwarded_traffic', 'allow_virtual_network_access', 'allow_gateway_transit']:
+            if locals()[bool_opt] != peering.get(bool_opt):
+                ret['changes'][bool_opt] = {
+                    'old': peering.get(bool_opt),
+                    'new': locals()[bool_opt]
+                }
+
+        if not ret['changes']:
+            ret['result'] = True
+            ret['comment'] = 'Peering object {0} is already present.'.format(name)
+            return ret
+
+        if __opts__['test']:
+            ret['result'] = None
+            ret['comment'] = 'Peering object {0} would be updated.'.format(name)
+            return ret
+
+    else:
+        ret['changes'] = {
+            'old': {},
+            'new': {
+                'name': name,
+                'remote_virtual_network': remote_virtual_network,
+                'remote_vnet_group': (remote_vnet_group or resource_group),
+                'use_remote_gateways': use_remote_gateways, 
+                'allow_forwarded_traffic': allow_forwarded_traffic, 
+                'allow_virtual_network_access': allow_virtual_network_access, 
+                'allow_gateway_transit': allow_gateway_transit
+            }
+        }
+
+    if __opts__['test']:
+        ret['comment'] = 'Subnet {0} would be created.'.format(name)
+        ret['result'] = None
+        return ret
+
+    peering_kwargs = kwargs.copy()
+    peering_kwargs.update(connection_auth)
+
+    peering = __salt__['azurearm_network.virtual_network_peering_create_or_update'](
+        name=name,
+        remote_virtual_network=remote_virtual_network,
+        remote_vnet_group=remote_vnet_group,
+        virtual_network=virtual_network,
+        resource_group=resource_group,
+        use_remote_gateways=use_remote_gateways, 
+        allow_forwarded_traffic=allow_forwarded_traffic, 
+        allow_virtual_network_access=allow_virtual_network_access, 
+        allow_gateway_transit=allow_gateway_transit,
+        **peering_kwargs
+    )
+
+    # This is a special case where one side of the peering has been deleted and recreated.
+    # In order to establish the new peer, the remote peer needs to be set back to "Initiated" state.
+    if peering.get('error', '').startswith('Azure Error: RemotePeeringIsDisconnected'):
+        rname_match = re.search('because remote peering (\S+) referencing parent virtual network', peering['error'])
+        remote_name = rname_match.group(1).split('/')[-1]
+
+        remote_peering = __salt__['azurearm_network.virtual_network_peering_get'](
+            name=remote_name,
+            virtual_network=remote_virtual_network,
+            resource_group=remote_vnet_group,
+            azurearm_log_level='info',
+            **connection_auth
+        )
+
+        remote_peering_kwargs = remote_peering.copy()
+        remote_peering_kwargs.update(connection_auth)
+        remote_peering_kwargs['peering_state'] = 'Initiated'
+        remote_peering_kwargs.pop('remote_virtual_network')
+
+        remote_peering = __salt__['azurearm_network.virtual_network_peering_create_or_update'](
+            remote_virtual_network=virtual_network,
+            remote_vnet_group=resource_group,
+            virtual_network=remote_virtual_network,
+            resource_group=remote_vnet_group,
+            **remote_peering_kwargs
+        )
+
+        peering = __salt__['azurearm_network.virtual_network_peering_create_or_update'](
+            name=name,
+            remote_virtual_network=remote_virtual_network,
+            remote_vnet_group=remote_vnet_group,
+            virtual_network=virtual_network,
+            resource_group=resource_group,
+            use_remote_gateways=use_remote_gateways, 
+            allow_forwarded_traffic=allow_forwarded_traffic, 
+            allow_virtual_network_access=allow_virtual_network_access, 
+            allow_gateway_transit=allow_gateway_transit,
+            **peering_kwargs
+        )
+
+    if 'error' not in peering:
+        ret['result'] = True
+        ret['comment'] = 'Peering object {0} has been created.'.format(name)
+        return ret
+
+    ret['comment'] = 'Failed to create peering object {0}! ({1})'.format(name, peering.get('error'))
+    return ret
+
+
+def virtual_network_peering_absent(name, virtual_network, resource_group, connection_auth=None):
+    '''
+    .. versionadded:: Sodium
+
+    Ensure a virtual network peering object does not exist in the virtual network.
+
+    :param name:
+        Name of the peering object.
+
+    :param virtual_network:
+        Name of the existing virtual network containing the peering object.
+
+    :param resource_group:
+        The resource group assigned to the virtual network.
+
+    :param connection_auth:
+        A dict with subscription and authentication parameters to be used in connecting to the
+        Azure Resource Manager API.
+    '''
+    ret = {
+        'name': name,
+        'result': False,
+        'comment': '',
+        'changes': {}
+    }
+
+    if not isinstance(connection_auth, dict):
+        ret['comment'] = 'Connection information must be specified via connection_auth dictionary!'
+        return ret
+
+    peering = __salt__['azurearm_network.virtual_network_peering_get'](
+        name,
+        virtual_network,
+        resource_group,
+        azurearm_log_level='info',
+        **connection_auth
+    )
+
+    if 'error' in peering:
+        ret['result'] = True
+        ret['comment'] = 'Peering object {0} was not found.'.format(name)
+        return ret
+
+    elif __opts__['test']:
+        ret['comment'] = 'Peering object {0} would be deleted.'.format(name)
+        ret['result'] = None
+        ret['changes'] = {
+            'old': peering,
+            'new': {},
+        }
+        return ret
+
+    deleted = __salt__['azurearm_network.virtual_network_peering_delete'](
+        name,
+        virtual_network,
+        resource_group,
+        **connection_auth
+    )
+
+    if deleted:
+        ret['result'] = True
+        ret['comment'] = 'Peering object {0} has been deleted.'.format(name)
+        ret['changes'] = {
+            'old': peering,
+            'new': {}
+        }
+        return ret
+
+    ret['comment'] = 'Failed to delete peering object {0}!'.format(name)
     return ret
