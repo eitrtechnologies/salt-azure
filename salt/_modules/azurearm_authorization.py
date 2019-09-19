f0f1052ace71ba032dc50350c60887958402b87d
Ajnbro <50883410+Ajnbro@users.noreply.github.com>
2019-09-19 04:45:03 +0000

    Finished up the get and list modules for each of the operations

--- salt/_modules/azurearm_authorization.py
+++ salt/_modules/azurearm_authorization.py
@@ -124,7 +124,7 @@ def provider_operations_metadata_list(**kwargs):
     '''
     result = {}
     authconn = __utils__['azurearm.get_client']('authorization', **kwargs)
-    
+
     try:
         providers = __utils__['azurearm.paged_object_to_list'](
             authconn.provider_operations_metadata.list(
@@ -227,36 +227,34 @@ def permissions_list_for_resource_group(name, **kwargs):
     return result
 
 
-def role_definitions_get(scope, id, **kwargs):
+def role_definitions_get(id, scope, **kwargs):
     '''
     .. versionadded:: Sodium
 
     Get role definition by name (GUID).
 
-    :param scope: The scope of the role definition.
-
     :param id: The ID of the role definition.
+
+    :param scope: The scope of the role definition.
 
     CLI Example:
 
     .. code-block:: bash
 
-        salt-call azurearm_authorization.role_definitions_get testscope testid
+        salt-call azurearm_authorization.role_definitions_get testid testscope
 
     '''
     result = {}
     authconn = __utils__['azurearm.get_client']('authorization', **kwargs)
 
     try:
-        defs = __utils__['azurearm.paged_object_to_list'](
-            authconn.role_definitions.get(
-                scope=scope,
-                role_definition_id=id,
-                **kwargs
-            )
+        defs = authconn.role_definitions.get(
+            scope=scope,
+            role_definition_id=id,
+            **kwargs
         )
 
-        result = defs
+        result = defs.as_dict()
     except CloudError as exc:
         __utils__['azurearm.log_cloud_error']('authorization', str(exc), **kwargs)
         result = {'error': str(exc)}
@@ -270,9 +268,9 @@ def role_definitions_get_by_id(id, **kwargs):
 
     Gets a role definition by ID.
 
-    :param id: The fully qualified role definition ID. Use the format, 
-        /subscriptions/{guid}/providers/Microsoft.Authorization/roleDefinitions/{roleDefinitionId} for subscription 
-        level role definitions, or /providers/Microsoft.Authorization/roleDefinitions/{roleDefinitionId} for tenant 
+    :param id: The fully qualified role definition ID. Use the format,
+        /subscriptions/{guid}/providers/Microsoft.Authorization/roleDefinitions/{roleDefinitionId} for subscription
+        level role definitions, or /providers/Microsoft.Authorization/roleDefinitions/{roleDefinitionId} for tenant
         level role definitions.
 
     CLI Example:
@@ -286,14 +284,12 @@ def role_definitions_get_by_id(id, **kwargs):
     authconn = __utils__['azurearm.get_client']('authorization', **kwargs)
 
     try:
-        defs = __utils__['azurearm.paged_object_to_list'](
-            authconn.role_definitions.get_by_id(
-                role_definition_id=id,
-                **kwargs
-            )
+        defs = authconn.role_definitions.get_by_id(
+            role_definition_id=id,
+            **kwargs
         )
 
-        result = defs
+        result = defs.as_dict()
     except CloudError as exc:
         __utils__['azurearm.log_cloud_error']('authorization', str(exc), **kwargs)
         result = {'error': str(exc)}
@@ -336,3 +332,224 @@ def role_definitions_list(scope, **kwargs):
     return result
 
 
+def role_assignments_get(name, scope, **kwargs):
+    '''
+    .. versionadded:: Sodium
+
+    Get the specified role assignment.
+
+    :param name: The name of the role assignment to get.
+
+    :param scope: The scope of the role assignment.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt-call azurearm_authorization.role_assignments_get testname testscope
+
+    '''
+    result = {}
+    authconn = __utils__['azurearm.get_client']('authorization', **kwargs)
+
+    try:
+        assigns = authconn.role_assignments.get(
+            role_assignment_name=name,
+            scope=scope,
+            **kwargs
+        )
+
+        result = assigns.as_dict()
+    except CloudError as exc:
+        __utils__['azurearm.log_cloud_error']('authorization', str(exc), **kwargs)
+        result = {'error': str(exc)}
+
+    return result
+
+
+def role_assignments_get_by_id(id, **kwargs):
+    '''
+    .. versionadded:: Sodium
+
+    Gets a role assignment by ID.
+
+    :param id: The fully qualified ID of the role assignment, including the scope, resource name and resource type.
+        Use the format, /{scope}/providers/Microsoft.Authorization/roleAssignments/{roleAssignmentName}. Example:
+        /subscriptions/{subId}/resourcegroups/{rgname}//providers/Microsoft.Authorization/roleAssignments/{roleAssignmentName}.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt-call azurearm_authorization.role_assignments_get_by_id testid
+
+    '''
+    result = {}
+    authconn = __utils__['azurearm.get_client']('authorization', **kwargs)
+
+    try:
+        assigns = authconn.role_assignments.get_by_id(
+            role_assignment_id=id,
+            **kwargs
+            )
+
+        result = assigns.as_dict()
+    except CloudError as exc:
+        __utils__['azurearm.log_cloud_error']('authorization', str(exc), **kwargs)
+        result = {'error': str(exc)}
+
+    return result
+
+
+def role_assignments_list(**kwargs):
+    '''
+    .. versionadded:: Sodium
+
+    Gets all role assignments for the subscription.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt-call azurearm_authorization.role_assignments_list
+
+    '''
+    result = {}
+    authconn = __utils__['azurearm.get_client']('authorization', **kwargs)
+
+    try:
+        assigns = __utils__['azurearm.paged_object_to_list'](
+            authconn.role_assignments.list(
+                filter=kwargs.get('filter'),
+                **kwargs
+            )
+        )
+
+        result = assigns
+    except CloudError as exc:
+        __utils__['azurearm.log_cloud_error']('authorization', str(exc), **kwargs)
+        result = {'error': str(exc)}
+
+    return result
+
+
+def role_assignments_list_for_resource(name, resource_group, resource_provider_namespace, resource_type,
+                                      parent_resource_path=None, **kwargs):
+    '''
+    .. versionadded:: Sodium
+
+    Gets all role assignments for a resource.
+
+    :param name: The name of the resource to get role assignments for.
+
+    :param resource_group: The name of the resource group.
+
+    :param resource_provider_namespace: The namespace of the resource provider.
+
+    :param resource_type: The resource type of the resource.
+
+    :param parent_resource_path: The parent resource identity.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt-call azurearm_authorization.role_assignments_list_for_resource testname testgroup testnamespace \
+                  testtype testpath
+
+    '''
+    result = {}
+    authconn = __utils__['azurearm.get_client']('authorization', **kwargs)
+
+    if parent_resource_path is None:
+        parent_resource_path = ''
+
+    try:
+        assigns = __utils__['azurearm.paged_object_to_list'](
+            authconn.role_assignments.list_for_resource(
+                resource_name=name,
+                resource_group_name=resource_group,
+                resource_provider_namespace=resource_provider_namespace,
+                resource_type=resource_type,
+                parent_resource_path=parent_resource_path,
+                filter=kwargs.get('filter'),
+                **kwargs
+            )
+        )
+
+        result = assigns
+    except CloudError as exc:
+        __utils__['azurearm.log_cloud_error']('authorization', str(exc), **kwargs)
+        result = {'error': str(exc)}
+
+    return result
+
+
+def role_assignments_list_for_resource_group(name, **kwargs):
+    '''
+    .. versionadded:: Sodium
+
+    Gets all role assignments for a resource group.
+
+    :param name: The name of the resource group.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt-call azurearm_authorization.role_assignments_list_for_resource_group testgroup
+
+    '''
+    result = {}
+    authconn = __utils__['azurearm.get_client']('authorization', **kwargs)
+
+    try:
+        assigns = __utils__['azurearm.paged_object_to_list'](
+            authconn.role_assignments.list_for_resource_group(
+                resource_group_name=name,
+                filter=kwargs.get('filter'),
+                **kwargs
+            )
+        )
+
+        result = assigns
+    except CloudError as exc:
+        __utils__['azurearm.log_cloud_error']('authorization', str(exc), **kwargs)
+        result = {'error': str(exc)}
+
+    return result
+
+
+def role_assignments_list_for_scope(scope, **kwargs):
+    '''
+    .. versionadded:: Sodium
+
+    Gets role assignments for a scope.
+
+    :param scope: The scope of the role assignments.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt-call azurearm_authorization.role_assignments_list_for_scope testscope
+
+    '''
+    result = {}
+    authconn = __utils__['azurearm.get_client']('authorization', **kwargs)
+
+    try:
+        assigns = __utils__['azurearm.paged_object_to_list'](
+            authconn.role_assignments.list_for_scope(
+                scope=scope,
+                filter=kwargs.get('filter'),
+                **kwargs
+            )
+        )
+
+        result = assigns
+    except CloudError as exc:
+        __utils__['azurearm.log_cloud_error']('authorization', str(exc), **kwargs)
+        result = {'error': str(exc)}
+
+    return result
