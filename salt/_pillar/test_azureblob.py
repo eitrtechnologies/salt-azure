f9c624b83a5537f8121f2a664f15314571d84180
Ajnbro <50883410+Ajnbro@users.noreply.github.com>
2019-12-21 18:33:44 +0000

    Adding additional tests

--- salt/_pillar/test_azureblob.py
+++ salt/_pillar/test_azureblob.py
@@ -1,10 +1,18 @@
+# -*- coding: utf-8 -*-
+'''
+Tests for the Azure Blob Ext_Pillar
+'''
+
 # Import python libs
 from __future__ import absolute_import, print_function, unicode_literals
+
+import os
 import tempfile
 
 # Import Salt Testing libs
 from tests.support.unit import TestCase, skipIf
-from tests.support.mock import NO_MOCK, NO_MOCK_REASON, patch, MagicMock
+from tests.support.mock import patch, MagicMock
+from tests.support.mixins import LoaderModuleMockMixin
 
 # Import Salt Libs
 from salt.ext import six
@@ -19,8 +27,6 @@ try:
     HAS_LIBS = True
 except ImportError:
     pass
-
-__opts__ = salt.config.minion_config('/etc/salt/minion')
 
 class MockBlob(dict):
     name = ''
@@ -47,10 +53,61 @@ class MockBlobServiceClient:
         container_client = MockContainerClient()
         return container_client
 
-@skipIf(NO_MOCK, NO_MOCK_REASON)
-@skipIf(not azureblob.HAS_LIBS, 'Azure modules not available')
-class AzureblobTestCase(TestCase):
-    # Params: connection_string, container, cache_file, multiple_env=False, environment='base'
+@skipIf(HAS_LIBS is False, 'The azure.storage.blob module must be installed.')
+class AzureBlobTestCase(TestCase, LoaderModuleMockMixin):
+    '''
+    TestCase for salt.pillar.azureblob ext_pillar.
+
+    NOTE: In an actual use case of the Azure Blob ext_pillar, the ext_pillar will be cached to the master. However,
+        for testing purposes the ext_pillar is cached to the minion.
+    '''
+
+    def setup_loader_modules(self):
+        '''
+        Setup loader modules
+        '''
+        self.opts = salt.config.DEFAULT_MINION_OPTS.copy()
+        utils = salt.loader.utils(self.opts)
+        funcs = salt.loader.minion_mods(self.opts, utils=utils)
+        return {
+            azureblob: {
+                '__opts__': self.opts, 
+                '__utils__': utils,
+                '__salt__': funcs
+            },
+        }
+
+    def setUp(self):
+        '''
+        Setup
+        '''
+        TestCase.setUp(self)
+        azureblob.__virtual__()
+
+
+    def test__init(self):
+        self.assertEqual('true', 'true')
+
+
+    def test__get_cache_dir(self):
+        ret = azureblob._get_cache_dir()
+        self.assertEqual(ret, '/var/cache/salt/minion/pillar_azureblob')
+
+
+    def test__get_cached_file_name(self):
+        container = 'test'
+        saltenv = 'base'
+        path = 'base/secret.sls'
+        ret = azureblob._get_cached_file_name(container, saltenv, path)
+        self.assertEqual(ret, '/var/cache/salt/minion/pillar_azureblob/base/test/base/secret.sls')
+
+
+    def test__get_containers_cache_filename(self):
+        container = 'test'
+        ret = azureblob._get_containers_cache_filename(container)
+        self.assertEqual(ret, '/var/cache/salt/minion/pillar_azureblob/test-files.cache')
+
+
     def test__refresh_containers_cache_file(self):
         blob_client = MockBlobServiceClient()
         container = 'test'
@@ -59,46 +116,65 @@ class AzureblobTestCase(TestCase):
             ret = azureblob._refresh_containers_cache_file('', container, cache_file.name)
             self.assertEqual(ret, {'base': {'test': [{'container': None, 'name': 'test.sls', 'prefix': None, 
                                    'delimiter': '/', 'results_per_page': None, 'location_mode': None}]}})
-    
-    # Params: connection_string, metadata, saltenv, container, path, cached_file_path)
+
+
+    # DO I NEED TO PATCH THE TEMP FILE TO THE METHOD?
+    # If I use the tempfile and use delete=False option, then where would the temp file be stored?
+    # https://stackoverflow.com/questions/8577137/how-can-i-create-a-tmp-file-in-python
+    def test__read_containers_cache_fle(self):
+        cache_file = tempfile.NamedTemporaryFile()
+        cache_file.name = 'test-files.cache'
+        #cache_file = '/var/cache/salt/minion/pillar_azureblob/test-files.cache'
+        #ret = azureblob._read_containers_cache_file(cache_file.name)
+        self.assertEqual('true', 'true')
+
+
+    def test__find_files(self):
+        metadata = {'test': [{'name': 'base/secret.sls'}, {'name': 'blobtest.sls', 'irrelevant': 'ignore.sls'},                                      {'name': 'base/'}]}
+        ret = azureblob._find_files(metadata)
+        self.assertEqual(ret, {'test': ['base/secret.sls', 'blobtest.sls']})
+
+
+    def test__find_file_meta1(self):
+        metadata = {'base': {'test': [{'name': 'base/secret.sls', 'relevant': 'include.sls'}, 
+                                      {'name': 'blobtest.sls', 'irrelevant': 'ignore.sls'}]}}
+        container = 'test'
+        saltenv = 'base'
+        path = 'base/secret.sls'
+        ret = azureblob._find_file_meta(metadata, container, saltenv, path)
+        self.assertEqual(ret, {'name': 'base/secret.sls', 'relevant': 'include.sls'})
+
+
+    def test__find_file_meta2(self):
+        metadata = {'wrong': {'test': [{'name': 'base/secret.sls'}]}}
+        container = 'test'
+        saltenv = 'base'
+        path = 'base/secret.sls'
+        ret = azureblob._find_file_meta(metadata, container, saltenv, path)
+        self.assertEqual(ret, None)
+
+
+    def test__find_file_meta3(self):
+        metadata = {'base': {'wrong': [{'name': 'base/secret.sls'}]}}
+        container = 'test'
+        saltenv = 'base'
+        path = 'base/secret.sls'
+        ret = azureblob._find_file_meta(metadata, container, saltenv, path)
+        self.assertEqual(ret, None)
+
+
+    # Can this be tested? If so, how?
     def test__get_file_from_blob(self):
         blob_client = MockBlobServiceClient()
+        metadata = {}
+        saltenv = 'base'
+        container = 'test'
+        path = ''
+        cached_file_path = ''
         '''
-        # Metadata
-        # Saltenv 
-        # Container
-        # Path
-        # Cached_file_path
         with patch.object(BlobServiceClient, 'from_connection_string', MagicMock(return_value=blob_client)):
             ret = azureblob._get_file_from_blob('', container, cache_file.name)
             self.assertEqual(ret, {'base': {'test': [{'container': None, 'name': 'test.sls', 'prefix': None,
-                                   'delimiter': '/', 'results_per_page': None, 'location_mode': None}]}})
-
+            'delimiter': '/', 'results_per_page': None, 'location_mode': None}]}})
         '''
-        pass
-
-
-    def test__get_cache_dir(self):
-        ret = azureblob._get_cache_dir()
-        self.assertEqual(ret, '/var/cache/salt/master/pillar_azureblob')
-
-
-    def test__get_cached_file_name(self):
-        ret = azureblob._get_cached_file_name('test', 'base', 'base/secret.sls')
-        self.assertEqual(ret, '/var/cache/salt/master/pillar_azureblob/base/test/base/secret.sls')
-
-
-    def test__get_containers_cache_filename(self):
-        ret = azureblob._get_containers_cache_filename('test')
-        self.assertEqual(ret, '/var/cache/salt/master/pillar_azureblob/test-files.cache')
-
-
-    def test__read_containers_cache_fle(self):
-        #ret = azureblob._read_containers_cache_file()
-        #self.assertEqual(ret, 'RIGHT THIS VALUE STILL')
-        pass
-
-    def test__find_files(self):
-        metadata = {'test': [{'name': 'base/secret.sls'}, {'name': 'blobtest.sls'}]}
-        ret = azureblob._find_files(metadata)
-        self.assertEqual(ret, {'test': ['base/secret.sls', 'blobtest.sls']})
+        self.assertEqual('true', 'true')
